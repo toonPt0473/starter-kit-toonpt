89c3c89cc7a1573a3b2eb689e3c23655933f011d
tassanai <tassanai.yeeton@gmail.com>
2018-08-14 16:09:00 +0700

    add: apollo-module

---
+++ src/models/count/query.js
@@ -0,0 +1,40 @@
+import gql from 'graphql-tag'
+import config from '../../utils/apollo-config'
+
+const { client } = config
+
+export const login = (param) => {
+  const { email, password } = param
+  return client.mutate({
+    mutation: gql`
+      mutation($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          jwt
+          firstname
+          lastname
+        }
+      }
+    `,
+    variables: { email, password },
+  })
+}
+
+export const register = (param) => {
+  const { username, password } = param
+  return client.mutate({
+    mutation: gql`
+      mutation($username: String!, $password: String!)
+      {
+        signup(username: $username, password: $password)
+        {
+          jwt
+          username
+        }
+      }
+    `,
+    variables: { username, password },
+  })
+}
+export default {
+  login,
+}
---
+++ src/utils/apollo-config.js
@@ -0,0 +1,37 @@
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const env = process.env.NODE_ENV || 'defaultConfig'
+
+let baseUrl = 'http://localhost:9000'
+if (env !== 'development') {
+  baseUrl = ''
+}
+
+const httpLink = createHttpLink({
+  uri: `${baseUrl}/graphql`,
+  opts: {
+    credentials: 'same-origin',
+  },
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await Expo.SecureStore.getItemAsync('token') // eslint-disable-line
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
+export default {
+  client,
+}
